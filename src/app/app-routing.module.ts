15ebe55ffa3acc048077a9358aaf395c4e36677b
doaa51094 <112436865+doaa51094@users.noreply.github.com>
2024-10-04 22:54:35 +0300

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,17 +10,18 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ProductComponent } from './product/product.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
   {path:"",redirectTo:'home',pathMatch:"full"},
-  {path:"home",component:HomeComponent , title:"home"},
-  {path:"about",component:AboutComponent , title:"about"},
-  {path:"brands",component:BrandComponent , title:"brands"},
-  {path:"category",component:CategoriesComponent , title:"category"},
-  {path:"footer",component:FooterComponent , title:"footer"},
-  {path:"cart",component:CartComponent , title:"cart"},
-  {path:"product",component:ProductComponent , title:"product"},
+  {path:"home",component:HomeComponent,canActivate:[AuthGuard] , title:"home"},
+  {path:"about",component:AboutComponent ,canActivate:[AuthGuard] , title:"about"},
+  {path:"brands",component:BrandComponent ,canActivate:[AuthGuard] , title:"brands"},
+  {path:"category",component:CategoriesComponent , canActivate:[AuthGuard] ,title:"category"},
+  {path:"footer",component:FooterComponent ,canActivate:[AuthGuard] , title:"footer"},
+  {path:"cart",component:CartComponent ,canActivate:[AuthGuard] , title:"cart"},
+  {path:"product",component:ProductComponent ,canActivate:[AuthGuard] , title:"product"},
 
 
   {path:"login",component:LoginComponent , title:"login"},
